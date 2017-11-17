d6d313d63f466952e273fed2032655f067829996
blockchainy <therulerguy@gmail.com>
2017-11-17 10:36:23 -0800

    buyer frontend

--- src/components/Buyer.js
+++ src/components/Buyer.js
@@ -6,34 +6,84 @@ import 'semantic-ui-css/semantic.min.css';
 
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-var myContract = web3.eth.contract([{ "constant": false, "inputs": [{ "name": "_amount", "type": "uint256" }], "name": "setAmount", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_time", "type": "uint256" }], "name": "setTime", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [], "name": "kill", "outputs": [{ "name": "", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getTime", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_price", "type": "uint256" }], "name": "setPrice", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPrice", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "checkBalance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getAmount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }])
-var contractInstance = myContract.at('0x486351a2b3f3f63c04aeec19281cc7293cab8337');
+var abi = [{"constant":true,"inputs":[],"name":"returnWeiInWallet","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"supply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"buySingleTicket","outputs":[{"name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[{"name":"_amount","type":"uint256"}],"name":"buyMultipleTickets","outputs":[{"name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"getAddress","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_supply","type":"uint256"}],"name":"setSupply","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"howMuchEtherAtAddress","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_address","type":"address"}],"name":"numberOfTicketFromAddress","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_price","type":"uint256"}],"name":"setPrice","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPrice","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fallBackCalled","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"howMuchEtherInContract","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"}]
+var myContract = web3.eth.contract(abi);
+var contractInstance = myContract.at('0xeefad58c2fb61220c12ea5a2ec5c808050a41c14');
 
-web3.eth.defaultAccount = web3.eth.accounts[0];
+web3.eth.defaultAccount = web3.eth.accounts[1];
 
 class Buyer extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            etherInWallet: 0
+            address: '',
+            etherInWallet: 0,
+            amount: 0,
+            amountOfTickets: 0
         };
-
-        this.kill = this.kill.bind(this);
+        this.buySingleTicket = this.buySingleTicket.bind(this);
+        this.buyTickets = this.buyTickets.bind(this);
 
     }
 
-    kill() {
-        contractInstance.kill();
-        var balance = contractInstance.checkBalance();
-        console.log(balance);
+    buySingleTicket() {
+        contractInstance.buySingleTicket({ value: 1000000000000000000 });
+        this.setState({ amountOfTickets: contractInstance.numberOfTicketFromAddress(this.state.address) });
+
+        var weiInWallet = contractInstance.returnWeiInWallet();
+        var etherInWallet = weiInWallet / 1000000000000000000;
+        this.setState({ etherInWallet });
+    }
+
+    buyTickets() {
+        // should display price in wei
+        var price = contractInstance.getPrice() * this.state.amount;
+        var status = contractInstance.buyMultipleTickets(this.state.amount, {value: price});
+        this.setState({ amountOfTickets: contractInstance.numberOfTicketFromAddress(this.state.address) });
+
+        // update state of amount of ether in wallet 
+        var weiInWallet = contractInstance.returnWeiInWallet();
+        var etherInWallet = weiInWallet / 1000000000000000000;
+        this.setState({ etherInWallet });
+
+        // TODO =====RETURN ERROR CHECK BELOW========
+        // var status = contractInstance.buyMultipleTickets();
+        // if (status == true) {
+        //     console.log(`${contractInstance.numberOfTicketFromAddress(this.state.address)} amount of tickets purchased!`);
+        // } else {
+        //     // should display error
+        //     console.log('Please have enough ether to buy amount of tickets specified');
+        // }
+    }
+
+    // get amount of ether in users wallet before anything
+    componentWillMount() {
+        // get wallet address
+        var address = contractInstance.getAddress();
+        // TODO: see if you can combine setState
+        this.setState({ address });
+
+        var weiInWallet = contractInstance.returnWeiInWallet();
+        var etherInWallet = weiInWallet / 1000000000000000000;
+        this.setState({ etherInWallet });
+
+        var amountOfTickets = contractInstance.numberOfTicketFromAddress(address);
+        this.setState({ amountOfTickets });
     }
 
     render() {
         return (
             <div>
-                <button class="ui animated button" tabindex="0" onClick={this.handleSubmit}>
-                    <div class="visible content">Purchase single ticket</div>
+                <div>
+                    Number of Tickets:
+                     <input
+                        onChange={event => this.setState({ amount: event.target.value })}
+                    />
+                </div>
+                <p></p>
+                <button class="ui animated button" tabindex="0" onClick={this.buyTickets}>
+                    <div class="visible content">Purchase ticket</div>
                     <div class="hidden content">
                         <i class="right arrow icon"></i>
                     </div>
